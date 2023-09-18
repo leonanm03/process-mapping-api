553d8abcfad0a6d491a17160635a333a1478e839
Leonan Martins <leonanm04@gmail.com>
2023-09-18 14:32:48 -0300

    feat: init processes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AreasModule } from './modules/areas/areas.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProcessesModule } from './modules/processes/processes.module';
 
 @Module({
-  imports: [AreasModule, PrismaModule],
+  imports: [AreasModule, PrismaModule, ProcessesModule],
   controllers: [],
   providers: [],
 })
